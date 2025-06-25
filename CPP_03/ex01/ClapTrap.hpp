ee5291cbff88571dd364b0a8b2fcd5c24cf0db98
Narek <157054887+nkarapet42@users.noreply.github.com>
2025-06-25 19:21:52 +0400

    Update ClapTrap.hpp

--- CPP_03/ex01/ClapTrap.hpp
+++ CPP_03/ex01/ClapTrap.hpp
@@ -12,7 +12,7 @@ public:
 	ClapTrap(const std::string& name);
 	ClapTrap(const ClapTrap& other);
 	ClapTrap& operator=(const ClapTrap& other); 
-	~ClapTrap();
+	virtual ~ClapTrap();
 
 	void	attack( const std::string& target );
 	void	takeDamage( unsigned int amount );
